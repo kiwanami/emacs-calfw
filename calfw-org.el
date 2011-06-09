92e45de5592f477e5ff23551fbdf256c57f2fb95
SAKURAI Masashi <m.sakurai@kiwanami.net>
2011-06-09 14:58:57 +0900

    added: org-agenda integration.

---
+++ calfw-org.el
@@ -0,0 +1,126 @@
+;;; calfw-org.el --- calendar view for org-agenda
+
+;; Copyright (C) 2011  SAKURAI Masashi
+
+;; Author: SAKURAI Masashi <m.sakurai at kiwanami.net>
+;; Keywords: calendar
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Display org-agenda items in the calfw buffer.
+;; (Because I don't use the org-agenda mainly, 
+;; I hope someone continue integration with the org.)
+
+;; (require 'calfw-org)
+;; (cfw:install-org-schedules) 
+;; 
+;; M-x cfw:open-org-calendar
+
+;;; Code:
+
+(require 'calfw)
+(require 'org)
+(require 'org-agenda)
+
+(defun cfw:org-collect-schedules-period (begin end &rest args)
+  "[internal] Return org schedule items between BEGIN and END. 
+ARGS is passed to `org-agenda-get-day-entries'."
+  (let ((org-agenda-prefix-format "")) ; ?
+    (org-compile-prefix-format nil)
+    (loop for date in (cfw:enumerate-days begin end) 
+          append
+          (loop for file in org-agenda-files 
+                append
+                (progn
+                  (org-check-agenda-file file)
+                  (apply 'org-agenda-get-day-entries file date args))))))
+
+(defun cfw:org-onclick ()
+  "Jump to the clicked org item."
+  (interactive)
+  (let ((marker (get-text-property (point) 'org-marker)))
+    (when marker
+      (switch-to-buffer (marker-buffer marker))
+      (goto-char (marker-position marker)))))
+
+(defvar cfw:org-text-keymap 
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mouse-1] 'cfw:org-onclick)
+    (define-key map (kbd "<return>") 'cfw:org-onclick)
+    map)
+  "key map on the calendar item text.")
+
+(defun cfw:org-summary-format (item)
+  "Format an item. (How should be displayed?)"
+  (let* ((time (get-text-property 0 'time item))
+         (category (get-text-property 0 'org-category item))
+         (tags (get-text-property 0 'tags item))
+         (marker (get-text-property 0 'org-marker item))
+         (buffer (marker-buffer marker)))
+    (propertize
+     (concat item " " (buffer-name buffer))
+     'keymap cfw:org-text-keymap)))
+
+(defvar cfw:org-schedule-summary-transformer 'cfw:org-summary-format
+  "Transformation function which transforms the org item string to calendar title.
+If this function splits into a list of string, the calfw displays those string in multi-lines.")
+
+(defun cfw:org-schedule-period-to-calendar (begin end)
+  "[internal] Return calfw calendar items between BEGIN and END
+from the org schedule data."
+  (loop 
+   for i in (cfw:org-collect-schedules-period begin end)
+        for date = (get-text-property 0 'date i)
+        for line = (funcall cfw:org-schedule-summary-transformer i)
+        with contents = nil
+        do (setq contents (cfw:contents-add date line contents))
+        finally return contents))
+
+(defvar cfw:org-schedule-map
+  (cfw:define-keymap
+   '(
+     ("q"   . kill-buffer)
+     ))
+  "Key map for the calendar buffer.")
+
+(defun cfw:open-org-calendar ()
+  "Open an org schedule calendar in the new buffer."
+  (interactive)
+  (switch-to-buffer
+   (cfw:get-calendar-buffer-custom
+    nil nil cfw:org-schedule-map)))
+
+(defun cfw:org-from-calendar ()
+  "Do something. This command should be executed on the calfw calendar."
+  (interactive)
+  (let* ((mdy (cfw:cursor-to-nearest-date))
+         (m (calendar-extract-month mdy))
+         (d (calendar-extract-day   mdy))
+         (y (calendar-extract-year  mdy)))
+    ;; exec org-remember here?
+    ))
+
+(defun cfw:install-org-schedules ()
+  "Add a schedule collection function to the calfw for the org."
+  (interactive)
+  (add-to-list 'cfw:contents-functions 'cfw:org-schedule-period-to-calendar))
+
+;; (cfw:install-org-schedules)
+;; (progn (eval-current-buffer) (cfw:open-calendar-buffer))
+;; (cfw:open-org-calendar)
+
+(provide 'calfw-org)
+;;; calfw-org.el ends here
